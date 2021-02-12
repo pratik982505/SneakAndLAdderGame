58559b9679a1726a918ae966524f6d09261f98e1
pratik982505 <gharatp%5@gmail.com>
2021-02-12 08:24:37 +0530

    PratikGharat Added:Code for PlayerStartingPostion

---
+++ SneakAndKLaddder.js
@@ -0,0 +1,10 @@
+console.log("Welcome to Sneak and Ladder Game:");
+
+class SneakAndLadder{
+     Position =()=>{
+         let playerPoasition = 0;
+         console.log("Player at Position: " + " "+playerPoasition);
+     }
+}
+var playerPosition = new SneakAndLadder() ;
+console.log(playerPosition.Position());
