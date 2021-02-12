44c713dad2206bc14d32c35684cc431617aec234
pratik982505 <gharatp%5@gmail.com>
2021-02-12 09:09:06 +0530

    PratikGharat Added:Code to Cheack Option NoPlay,Snake,ladder

--- SneakAndKLaddder.js
+++ SneakAndKLaddder.js
@@ -1,16 +1,36 @@
 console.log("Welcome to the Snake And Ladder Game..!!");
 
+let playerPosition = 0;
+
 class SnakeAndLadder {
-    Position = () => {
-        let playerPosition = 0;
-        return playerPosition;
-    }
+
     rollDice = () => {
         let diceRoll = Math.floor(Math.random() * 6) + 1;
-        console.log("Dice Number When Rolled : " + diceRoll);
+        console.log("Dice Rolled: " + diceRoll);
+        return diceRoll;
+    }
+
+    option = () => {
+        let optionDiceRoll = Math.floor(Math.random() * 3);
+        switch (optionDiceRoll) {
+            case 0:
+                console.log("No Play..!! Player at Position: " + playerPosition);
+                break;
+            case 1:
+                playerPosition += this.rollDice();
+                console.log("Ladder..!! Player at Position: " + playerPosition);
+                break;
+            case 2:
+                playerPosition -= this.rollDice();
+                if (playerPosition < 0) {
+                    playerPosition = 0;
+                }
+                console.log("Snake..!! Player at Position: " + playerPosition);
+                break;
+        }
     }
 }
-var playerPosition = new SnakeAndLadder() ;
-console.log(playerPosition.Position());
 var DiceRoll = new SnakeAndLadder();
 console.log(DiceRoll.rollDice());
+var Optionn = new SnakeAndLadder();
+console.log(Optionn.option());
