e530edbe3c5a64a7765aa08208f30defdd9b54fa
pratik982505 <gharatp%5@gmail.com>
2021-02-12 08:37:45 +0530

    PratikGharat Added :Code to Dice Roll using Random

--- SneakAndKLaddder.js
+++ SneakAndKLaddder.js
@@ -1,10 +1,16 @@
-console.log("Welcome to Sneak and Ladder Game:");
+console.log("Welcome to the Snake And Ladder Game..!!");
 
-class SneakAndLadder{
-     Position =()=>{
-         let playerPoasition = 0;
-         console.log("Player at Position: " + " "+playerPoasition);
-     }
+class SnakeAndLadder {
+    Position = () => {
+        let playerPosition = 0;
+        return playerPosition;
+    }
+    rollDice = () => {
+        let diceRoll = Math.floor(Math.random() * 6) + 1;
+        console.log("Dice Number When Rolled : " + diceRoll);
+    }
 }
-var playerPosition = new SneakAndLadder() ;
+var playerPosition = new SnakeAndLadder() ;
 console.log(playerPosition.Position());
+var DiceRoll = new SnakeAndLadder();
+console.log(DiceRoll.rollDice());
